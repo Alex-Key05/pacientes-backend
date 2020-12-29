a36147d3d0754a694d308fffecbc22bed113c487
Alex-Key05 <cgamer143@gmail.com>
2020-12-29 12:54:16 -0600

    Agrego CORS

--- API/index.js
+++ API/index.js
@@ -2,8 +2,27 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
+// Crear servidor
 const app = express();
+
+// Habilitar cors
+const whitelist = ['http:/localhost:3000'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        const existe = whitelist.some( dominio => dominio === origin );
+        if( existe ) {
+            callback(null, true)
+        } else {
+            callback( new Error('No permitido por CORS') )
+        }
+    }
+}
+
+// Habilitar cors
+app.use(cors());
+// app.use(cors(corsOptions));
 
 // Conectar a mongo
 mongoose.Promise = global.Promise;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,7 @@
+import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import clienteAxios from './config/axios';
+
 // Componentes
 import Pacientes from './components/Pacientes';
 import NuevaCita from './components/NuevaCita';
@@ -6,6 +9,22 @@ import Cita from './components/Cita';
 
 
 function App() {
+
+  // state de la app
+  const [citas, guardarCitas] = useState([]);
+
+  useEffect( () => {
+    const consultarAPI = () => {
+      clienteAxios('/pacientes') // http://localhost:4000/pacientes
+        .then(respuesta => {
+          guardarCitas(respuesta.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    } 
+    consultarAPI();
+  }, []);
 
   return (
     <div className="App">
