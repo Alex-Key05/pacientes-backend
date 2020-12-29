62ad2deb9d3e197b8a63afb6c4c2da24b8670420
Alex-Key05 <cgamer143@gmail.com>
2020-12-29 08:43:07 -0600

    Agrego Actualizar registro por su ID

--- API/controllers/pacienteControllers.js
+++ API/controllers/pacienteControllers.js
@@ -1,37 +1,48 @@
-const Paciente = require('../models/Paciente');
+const Paciente = require("../models/Paciente");
 
+exports.nuevoCliente = async (req, res, next) => {
+  const paciente = new Paciente(req.body);
 
-exports.nuevoCliente = async(req, res, next) => {
-    
-    const paciente = new Paciente(req.body);
-
-    try {
-        await paciente.save();
-        res.json({ mensaje: 'Paciente agregado correctamente' });
-    } catch(error) {
-        console.log(error);
-        next();
-    } 
-}
+  try {
+    await paciente.save();
+    res.json({ mensaje: "Paciente agregado correctamente" });
+  } catch (error) {
+    console.log(error);
+    next();
+  }
+};
 
 // Obtiene todos los pacientes
-exports.obtenerPacientes = async(req, res, next) => {
-    try {
-        const pacientes = await Paciente.find({});
-        res.json(pacientes)
-    } catch (error) {
-        console.log(error)
-        next();
-    }
-}
+exports.obtenerPacientes = async (req, res, next) => {
+  try {
+    const pacientes = await Paciente.find({});
+    res.json(pacientes);
+  } catch (error) {
+    console.log(error);
+    next();
+  }
+};
 
 // Obtiene un paciente específico por su ID
-exports.obtenerPaciente = async(req, res, next) => {
-    try {
-        const paciente = await Paciente.findById(req.params.id);
-        res.json(paciente);
-    } catch (error) {
-        console.log(error);
-        next();
-    }
-}
+exports.obtenerPaciente = async (req, res, next) => {
+  try {
+    const paciente = await Paciente.findById(req.params.id);
+    res.json(paciente);
+  } catch (error) {
+    console.log(error);
+    next();
+  }
+};
+
+// Actualiza un registro por su ID
+exports.actualizarPaciente = async (req, res, next) => {
+  try {
+    const paciente = await Paciente.findOneAndUpdate(
+      { _id: req.params.id }, req.body, { new: true, 
+    });
+    res.json(paciente);
+  } catch (error) {
+    console.log(error);
+    next();
+  }
+};
--- API/routes/index.js
+++ API/routes/index.js
@@ -4,16 +4,25 @@ const pacienteController = require('../controllers/pacienteControllers');
 const router = express.Router();
 
 module.exports = function() {
+
+    // Add a new patient
     router.post('/pacientes', 
         pacienteController.nuevoCliente
     );
 
+    // Obtiene todos los registros de pacientes en la DB
     router.get('/pacientes',
         pacienteController.obtenerPacientes
     );
 
+    // Obtener un paciente específico
     router.get('/pacientes/:id',
         pacienteController.obtenerPaciente
+    );
+
+    // Actualizar un registro con un ID específico
+    router.put('/pacientes/:id',
+        pacienteController.actualizarPaciente
     );
 
     return router;
